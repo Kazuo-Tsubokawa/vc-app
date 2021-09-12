3f76246e351af3aed843eb57c5979850c25ddf0a
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-09-12 14:26:29 +0900

    配列にRuby追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -8,7 +8,7 @@ class LanguageController extends Controller
 {
     public function index ()
     {
-        $languages = ['HTML','CSS','Javascript','PHP'];
+        $languages = ['HTML','CSS','Javascript','PHP','Ruby'];
         return view('language.index', ['languages' => $languages]);
     }
 }
