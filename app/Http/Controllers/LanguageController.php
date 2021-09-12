78b3adba8dfd4a0c9590e9e986cfa369921df5da
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-09-12 14:28:58 +0900

    Python追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -8,7 +8,7 @@ class LanguageController extends Controller
 {
     public function index ()
     {
-        $languages = ['HTML','CSS','Javascript','PHP'];
+        $languages = ['HTML','CSS','Javascript','PHP','Python'];
         return view('language.index', ['languages' => $languages]);
     }
 }
